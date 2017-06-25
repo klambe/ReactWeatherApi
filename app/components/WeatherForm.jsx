db6cf223f57e18195cdf53da7a370dabeb64255a
klambe17@gmail.com <klambe17@gmail.com>
2017-06-25 14:49:03 +0100

    updated project to fake api call with default prop and updated state from form

--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -1,12 +1,24 @@
 var React = require('react');
 
 var WeatherForm = React.createClass({
+  onFormSubmit: function(e){
+    e.preventDefault();
+
+    var location = this.refs.location.value;
+
+    if(location.length >0){
+      this.refs.location.value = '';
+      this.props.onSearch(location);
+    }
+
+
+  },
   render: function(){
     return(
       <div>
-        <form >
+        <form onSubmit={this.onFormSubmit}>
           <div>
-            <input type="text" ref="cityName" placeholder="Enter City Name"/>
+            <input type="text" ref="location" placeholder="Enter City Name"/>
           </div>
           <div>
             <button>Get Weather</button>
