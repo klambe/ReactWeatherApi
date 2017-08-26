511e127bae5ab9f503bbc6583a1a016754006483
Kevin <klambe17@gmail.com>
2017-08-26 19:03:16 +0100

    modified the about page

--- public/bundle.js
+++ public/bundle.js
@@ -27204,9 +27204,18 @@
 
 	var About = function About(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'About Component'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'About'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Welcome to the about page!'
+	        )
 	    );
 	};
 
@@ -27222,9 +27231,18 @@
 
 	var Examples = function Examples(props) {
 	    return React.createElement(
-	        'h3',
+	        'div',
 	        null,
-	        'Examples Component!'
+	        React.createElement(
+	            'h3',
+	            null,
+	            'Examples'
+	        ),
+	        React.createElement(
+	            'p',
+	            null,
+	            'Welcome to the examples page!'
+	        )
 	    );
 	};
 
