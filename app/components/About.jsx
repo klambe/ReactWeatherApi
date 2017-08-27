5a3b96d99e997ef634ad34e3f536069e4a169cab
Kevin <klambe17@gmail.com>
2017-08-27 15:44:23 +0100

    modified about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,21 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to the about page!</p>
+            <h1 className="text-center">About</h1>
+            <p>Welcome to the about page! This is a weather app built using react and follows the complete react guide on Udemy</p>
+
+            <p>Below are links to some useful information:</p>
+
+            <ul>
+                <li>
+                    <a href="https://github.com/klambe/ReactWeatherApi">Project Github Repo</a>
+                </li>
+
+                <li>
+                    <a href="https://whispering-everglades-75762.herokuapp.com">Heroku Page Link</a>
+                </li>
+
+            </ul>
         </div>
     )
 };
